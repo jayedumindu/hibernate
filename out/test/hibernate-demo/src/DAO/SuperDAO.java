4fa28b88b441fbf8e697a294af2f1c6506b97090
jayedumindu <jayedumindu@gmail.com>
2022-06-23 12:11:17 +0530

    added changes

--- out/test/hibernate-demo/src/DAO/SuperDAO.java
+++ out/test/hibernate-demo/src/DAO/SuperDAO.java
@@ -1,4 +1,20 @@
 package DAO;
 
-public interface SuperDAO {
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface SuperDAO<T,ID> {
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean  save(T entity) throws SQLException, ClassNotFoundException;
+
+    boolean update(T entity) throws SQLException, ClassNotFoundException;
+
+    T search(ID id)throws SQLException,ClassNotFoundException;
+
+    boolean exist(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    String generateNewID() throws SQLException, ClassNotFoundException;
 }
